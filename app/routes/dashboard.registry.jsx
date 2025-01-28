9a5ac5ab26c097f3a1d3dc7eebab65953e64cfc6
muhammadsubhankhan <ahsan.qureshi@bazaarvoice.com>
2025-01-28 17:49:54 +0500

    Fix (Dashboard Registry) - set id of registry in API call

--- app/routes/dashboard.registry.jsx
+++ app/routes/dashboard.registry.jsx
@@ -8,11 +8,11 @@ import {fetchProducts} from '~/graphql/product-query/GetProductsQuery';
 export async function loader({request, context}) {
   const registry = context?.session?.get('@Registry');
   const res = await context.ClientGet(
-    `registryProducts/${registry[0].id}?type=gift`,
+    `registryProducts/${registry.id}?type=gift`,
     context,
   );
   const cashRes = await context.ClientGet(
-    `registryProducts/${registry[0].id}?type=cash`,
+    `registryProducts/${registry.id}?type=cash`,
     context,
   );
   let mergedArray = [];
@@ -46,7 +46,7 @@ export async function loader({request, context}) {
 }
 
 const index = () => {
-  const { data, cashfundData } = useLoaderData();
+  const {data, cashfundData} = useLoaderData();
   return (
     <div className="max-w-4xl mx-auto p-4 bg-gray-100 border border-gray-300 rounded-lg">
       <h1 className="text-2xl font-bold mb-4">Registry Homepage</h1>
@@ -94,7 +94,7 @@ const index = () => {
 };
 
 export default index;
-const ProductPage = ({ data }) => {
+const ProductPage = ({data}) => {
   return (
     <div className="container p-4">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
