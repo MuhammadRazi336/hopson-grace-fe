3cf2078cba162d7cb0c9c1aeeb05f34e3450f406
qasimcodup <mujahidsoomro5@gmail.com>
2025-06-17 13:31:21 +0500

    Build Error --Resolved

--- app/routes/couple.test._index.jsx
+++ app/routes/couple.test._index.jsx
@@ -2,7 +2,7 @@ import {useCallback, useState} from 'react';
 import {defer, Form, redirect, useLoaderData} from '@remix-run/react';
 import {Link} from '@remix-run/react';
 import SideCart from '~/components/SideCart';
-import CoupleFooter from '~/components/CoupleFooter';
+import {CoupleFooter} from '~/components/CoupleFooter';
 
 export default function CoupleProfileView() {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
