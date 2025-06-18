032aad39a5ceb86273410f20206edda54b8ea1e9
qasimcodup <mujahidsoomro5@gmail.com>
2025-06-18 17:04:34 +0500

    checkout  responsive

--- app/routes/checkout._index.jsx
+++ app/routes/checkout._index.jsx
@@ -5,7 +5,7 @@ import {Footer} from '~/components/Footer';
 import ImageAndText from '~/components/ImageAndText';
 import teaImg from '/assets/Images/reading-image.png';
 import lineImg3 from '/assets/Images/line.png';
-
+import CheckoutSteps from '~/components/CheckoutSteps';
 const cartItems = [
   {
     name: 'Coluna Fruit Bowl',
@@ -47,28 +47,7 @@ export default function Checkout() {
         />
       </div>
 
-      <div className="max-w-4xl mx-auto mt-[80px]">
-        <div className="flex items-center justify-around">
-          <div className="w-4/12">
-            <h4 className="text-[60px] font-bold text-center prata">1.</h4>
-            <p className="text-lg max-w-24 mx-auto uppercase text-center">
-              Add your messsage
-            </p>
-          </div>
-          <div className="w-4/12">
-            <h4 className="text-[60px] font-bold text-center prata">2.</h4>
-            <p className="text-lg max-w-24 mx-auto uppercase text-center">
-              Billing & Payment
-            </p>
-          </div>
-          <div className="w-4/12">
-            <h4 className="text-[60px] font-bold text-center prata">3.</h4>
-            <p className="text-lg max-w-32 mx-auto uppercase text-center">
-              Order Confirmation
-            </p>
-          </div>
-        </div>
-      </div>
+      <CheckoutSteps />
 
       <div className="container mx-auto py-[100px]">
         <div className=" bg-[#446184]  py-16 px-16">
