ebf826d7b1eed75b6fce940678cd585c67be12fc
qasimcodup <mujahidsoomro5@gmail.com>
2025-06-26 12:48:01 +0500

    Refactor PreviewRegistry component for improved layout and styling. Adjust dimensions and spacing for better visual presentation.

--- app/components/PreviewRegistry.jsx
+++ app/components/PreviewRegistry.jsx
@@ -1,13 +1,13 @@
 function PreviewRegistry() {
   return (
-    <div className="absolute top-0 right-12 max-w-[260px] w-[20%] min-h-[160px]  bg-[#446184] z-10">
-      <div className="container  mx-auto pt-5">
+    <div className="absolute top-0 right-12 max-w-[200px] w-[20%] min-h-[100px] bg-[#446184] z-10">
+      <div className="container mx-auto pt-3">
         <img
           src="/assets/Images/share-icon.png"
           alt="preview"
-          className="w-12 mx-auto"
+          className="w-10 mx-auto"
         />
-        <h2 className="text-white text-xl text-center font-bold mt-3">
+        <h2 className="text-white text-sm text-center font-bold mt-2">
           PREVIEW MY <br /> REGISTRY
         </h2>
       </div>
