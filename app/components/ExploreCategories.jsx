55262894660f25aa55b00edf6073b4d13ad5ee7f
qasimcodup <mujahidsoomro5@gmail.com>
2025-06-27 15:20:36 +0500

    Refactor ExploreCategories and ProductSection components for improved layout and responsiveness. Update styles for better visual hierarchy and add new sections for related products and actions, enhancing user experience.

--- app/components/ExploreCategories.jsx
+++ app/components/ExploreCategories.jsx
@@ -8,18 +8,18 @@ import product4 from '/assets/Images/gift-img-collection-4.png';
 
 function ExploreCategories() {
   return (
-    <section className="bg-[#FAF9F6] py-12 container mx-auto">
+    <section className="bg-[#FAF9F6] py-12  container mx-auto">
       <Heading
         text="explore more categories"
         classes={
           'prata text-2xl lg:text-4xl font-normal text-center max-[1024px]:m-0'
         }
         image={lineImghead}
-        imageClasses={'max-[1024px]:max-w-[330px]'}
+        imageClasses={'max-[1024px]:max-w-[330px] px-4 '}
       />
 
       {/* slides here */}
-      <div className="grid lg:grid-cols-3 grid-cols-2 gap-y-16 gap-x-6 mt-16 mx-10">
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-y-16 gap-x-6 mt-16 mx-10">
         <div>
           <img src={product1} alt="New Arrival" className="w-full" />
           <h3 className="mt-2.5 text-center lg:mt-[30px] uppercase lg:text-2xl text-sm font-medium tracking-wider">
