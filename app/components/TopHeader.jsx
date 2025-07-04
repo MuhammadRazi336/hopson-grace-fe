f260fd493d4d56e0a690507631ec817a232fdf5f
qasimcodup <mujahidsoomro5@gmail.com>
2025-07-04 17:20:20 +0500

    HGGR-219

--- app/components/TopHeader.jsx
+++ app/components/TopHeader.jsx
@@ -5,9 +5,9 @@ const TopHeader = () => {
         <div className="bg-black text-white py-[15px] text-center text-sm min[767px]:text-[20px] tracking-[4px]">
             <div className="container">
                 REGISTRY, REDEFINED 
-                <span className="px-2">|</span> 
-                <Link to="#" className="text-white px-1 tracking-[1px] font-bold">
-                    START YOUR JOURNEY <span className="text-[16px]">▶</span>
+                <span className="lg:px-2 px-1">|</span> 
+                <Link to="#" className="text-white px-1 lg:tracking-[1px] tracking-[0] font-bold">
+                    START YOUR JOURNEY <span className="lg:text-[16px] tracking-[1px] text-[14px]">▶</span>
                 </Link> 
             </div>
         </div>
