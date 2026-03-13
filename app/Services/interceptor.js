a0f4e54476cec0f55b9cb8e299ede430b6d8cdf7
MuhammadRazi336 <muhammad.razi@codup.co>
2026-03-13 16:16:04 +0500

    Update axios instance to append '/api/' to the base URL for improved API endpoint configuration

--- app/Services/interceptor.js
+++ app/Services/interceptor.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.API_BASE_URL, // Set your API URL here
-  // baseURL: 'http://localhost:3040/api/', // Set your API URL here
-
-  // https://dev-hopsongrace.codup.io/api/
+  baseURL: process.env.API_BASE_URL + '/api/', // Set your API URL here
   timeout: 20000,
 });
-
 // Request Interceptor
 axiosInstance.interceptors.request.use(
   (config, token) => {
