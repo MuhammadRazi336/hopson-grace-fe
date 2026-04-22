1c4d4db0af6992b34dc55ecbddf3f51cccd8f7c7
DESKTOP-BVMGFO0\shahab.majeed_codup <shahab.majeed@codup.co>
2026-04-22 20:15:52 +0500

    filter

--- app/routes/products.bestsellers.jsx
+++ app/routes/products.bestsellers.jsx
@@ -54,7 +54,7 @@ function SidebarFilter({
 }) {
   const [openSections, setOpenSections] = useState({
     categories: true,
-    styles: false,
+    styles: true,
   });
 
   const parentCollections = collections.filter(isParentForSidebar);
--- app/routes/products.new-ins.jsx
+++ app/routes/products.new-ins.jsx
@@ -51,7 +51,7 @@ function SidebarFilter({
 }) {
   const [openSections, setOpenSections] = useState({
     categories: true,
-    styles: false,
+    styles: true,
   });
 
   const parentCollections = collections.filter(isParentForSidebar);
